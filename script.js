fa4d7f7492e8d14760b19c42599042dc7910f98c
Kristiyana Milcheva <k.milcheva@turbit.de>
2023-12-30 19:38:10 +0100

    added basic styling

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const targetDate = new Date("Jan 8, 2024 18:00:00").getTime();
+const TARGET_DATE = new Date("Jan 8, 2024 18:00:00").getTime();
+// const TARGET_DATE = new Date("Dec 30, 2023 19:25:00").getTime();
+const countdownImage = document.getElementById("wolf-bike");
 
+function getTimeLeft(milliseconds) {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const totalHours = Math.floor(totalMinutes / 60);
+    const days = Math.floor(totalHours / 24);
+
+    const hours = totalHours % 24;
+    const minutes = totalMinutes % 60;
+    const seconds = totalSeconds % 60;
+
+    return { days, hours, minutes, seconds };
+}
+
+function setTimerToExpired() {
+    clearInterval(interval);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+    countdownImage.style.filter = "grayscale(100%)";
+}
+
+function countdown(remainingTime) {
+    returnTimer(remainingTime)
+    decreaseColour(remainingTime)
+}
+
+function returnTimer(remainingTime) {
+    const { days, hours, minutes, seconds } = getTimeLeft(remainingTime);
+    const timer = document.getElementById("timer") 
+    timer.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`
+}
+
+function decreaseColour(remainingTime) {
+    const totalDuration = TARGET_DATE - new Date("Dec 30, 2023 00:00:00").getTime();
+    const saturation = (remainingTime / totalDuration) * 10000;
+    countdownImage.style.filter = `grayscale(${100 - saturation}%)`;
+}
 const updateCountdown = () => {
-    const now = new Date().getTime();
-    const distance = targetDate - now;
-
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(interval);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-    }
+    const remainingTime = TARGET_DATE - new Date().getTime();
+    remainingTime > 0 ? countdown(remainingTime) : setTimerToExpired()
 };
-
 const interval = setInterval(updateCountdown, 1000);
+  
+
+
