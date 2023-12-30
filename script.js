9e63f2306cc3777bdf4192d2d39a7cdf80e6c980
Kristiyana Milcheva <k.milcheva@turbit.de>
2023-12-30 18:00:04 +0100

    base files

---
+++ script.js
@@ -0,0 +1,21 @@
+const targetDate = new Date("Jan 8, 2024 18:00:00").getTime();
+
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+};
+
+const interval = setInterval(updateCountdown, 1000);
